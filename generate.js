2f16f051f195a3a4dcf44f467f218d6ff2befea5
IceAdventures <yetzabelandrade@gmail.com>
2026-03-28 09:20:27 -0600

    Cambios archivos nuevos

--- generate.js
+++ generate.js
@@ -1,1 +1,21 @@
-gne
+const fs = require("fs");
+const yaml = require("js-yaml");
+const Handlebars = require("handlebars");
+
+// Leer YAML
+const fileContents = fs.readFileSync("cv.yaml", "utf8");
+const data = yaml.load(fileContents);
+
+// Leer template
+const templateHtml = fs.readFileSync("template.html", "utf8");
+
+// Compilar template
+const template = Handlebars.compile(templateHtml);
+
+// Generar HTML final
+const result = template(data);
+
+// Guardar resultado
+fs.writeFileSync("output.html", result);
+
+console.log("CV generado: output.html");
